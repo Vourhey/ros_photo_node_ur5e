c215be2410450fa070bc57d8a9610737be41f254
morekitaicev <36542961+morekitaicev@users.noreply.github.com>
2020-02-10 19:35:02 +0300

    Add files via upload

--- photo_package/src/photo_node.py
+++ photo_package/src/photo_node.py
@@ -2,6 +2,7 @@
 import urllib
 import roslib
 import rospy
+import sys
 from std_msgs.msg import String
 
 ip = "http://192.168.1.100:8080/photoaf.jpg"
@@ -16,4 +17,7 @@ def listener():
     rospy.spin()
 
 if __name__ == '__main__':
+    global ip
+    if len(sys.argv) == 2:
+        ip = sys.argv[1]
     listener()
