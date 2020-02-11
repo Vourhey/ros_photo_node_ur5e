f00be8a56630bec2622d54b85b28ce9db59ad375
Vadim Manaenko <vadim.razorq@gmail.com>
2020-02-11 11:21:07 +0300

    OOP way

---
+++ photo_package/scripts/photo_node.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import urllib
+import roslib
+import rospy
+import sys
+from std_msgs.msg import Empty
+
+
+class PhotoNode:
+
+    def __init__(self):
+        rospy.init_node("photo_node", anonymous=True)
+        rospy.Subscriber("/photo", String, callback)
+
+        self.ip = rospy.get_param("endpoint", "http://192.168.31.128:8089/")
+        self.count = 0
+
+    def callback(self, data):
+        rospy.loginfo("Downloading image...")
+
+        filename = "photo-" + str(self.count) + ".jpg"
+        urllib.urlretrieve(self.ip + "photoaf.jpg",  filename)
+        self.count += 1
+
+        rospy.loginfo("Finished")
+
+    def spin(self):
+        rospy.spin()
+
+
+if __name__ == "__main__":
+    PhotoNode().spin()
+
